35b0dc493645fedc8faa50401f9376d90c532d41
Daniel Eguia <eguiadaniel@gmail.com>
2021-07-09 21:43:02 +0200

    Table-icon with horizontal scroll

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import { Box, Container } from '@material-ui/core';
+
+import TableIcon from './TableIcon';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container maxWidth="sm">
+        <h1>Hello</h1>
+      </Container>
+      <TableIcon></TableIcon>
     </div>
   );
 }
